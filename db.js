9454193bbdd0d15880e950134e427ecad4750092
Fulgencio <funche_em@hotmail.com>
2022-12-19 11:30:24 +0100

    act

--- db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-module.exports = function () {
-    mongoose.connect('mongodb://127.0.0.1:27017/nasa')
-    .then(() => console.log("Conectado a mongodb..."))
-    .catch(() => console.log("ERROR FATAL: ", err))
-}
-
---
+++ src/app.js
@@ -0,0 +1,26 @@
+const landings = require('./routes/landings')
+const neas = require('./routes/neas')
+const users = require('./routes/users')
+
+const express = require('express')
+
+require('dotenv').config()
+
+const app = express()
+
+require('./db')()
+
+app.use(express.json())
+
+app.use('/api/astronomy/landings', landings)
+app.use('/api/astronomy/neas', neas)
+app.use('/api/users', users)
+
+
+app.get('/ping', (req, res) => {
+    res.send('pong')
+})
+
+const port = 3000
+
+app.listen(port, () => console.log(`Servidor corriendo en http://localhost:${port}`))
